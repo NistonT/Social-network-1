46c1fd3f721466c7b4bd1486ac22cd6ff9658a25
NistonN <nisprogrammer@gmail.com>
2023-09-03 20:21:52 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://free-png.ru/wp-content/uploads/2022/01/free-png.ru-46.png" alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src="https://pm1.aminoapps.com/7054/a2e53fb0507027f5894d1f9004c8ecbb2c887847r1-1500-844v2_hq.jpg" alt="background" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my post
+        </div>
+        <div>
+          new post
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+      // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHFHFJarkXhzp-5D0c_vUFvSegpXsJEZikv9LdZdxdof1GXUt9rF3UgLnQ7Yvj87rT70k&usqp=CAU
+    );
+}
+
+export default Profile;
