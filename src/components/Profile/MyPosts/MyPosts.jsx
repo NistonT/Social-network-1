fcf3a815451ef42a6f0ff22d5589060f921aaf1d
NistonN <nisprogrammer@gmail.com>
2023-09-05 00:52:04 +0300

    5 commit

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,9 +3,9 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
+        <div className={style.myPost}>
           my posts
-          <div>
+          <div className={style.inputPost}>
             <textarea></textarea>
             <button>Add Post</button>
           </div>
